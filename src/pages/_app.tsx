941a589e8ac5dd60577eca8e22852628a72ba3f3
Iris <agboolaisholaidreez>
2022-11-28 09:40:26 +0100

    update

---
+++ src/pages/_app.tsx
@@ -0,0 +1,5 @@
+import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-300 min-h-screen">
+      <div className="container mx-auto">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vel
+          molestias aperiam pariatur quas neque voluptates modi, ad distinctio
+          hic incidunt quasi et voluptate blanditiis repudiandae ab nemo, dolor
+          voluptatibus.
+        </h1>
+      </div>
+    </div>
+  );
+}
